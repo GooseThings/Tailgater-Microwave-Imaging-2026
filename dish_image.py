7a4cf81e0829651f5ebfaf66cb83f8540ad8f0a7
GooseThings <levi.gates@gmail.com>
2026-02-27 19:11:42 -0500

    Update comments and version in dish_image.py

--- dish_image.py
+++ dish_image.py
@@ -1,18 +1,18 @@
-#Python program to process raw Tailgater scan data from dish_scan.py
-#Version 2.0
-#Takes saved text file (numpy array) and converts into heatmap
+# Python program to process raw Tailgater scan data from dish_scan.py
+# Version 2.0.1
+# Takes saved text file (numpy array) and converts into heatmap
 
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
 
-#Open the filename passed at runtime
-print('Loading data file.')
+# Open the filename passed at runtime
+print('Loading data file')
 with open(sys.argv[1], 'r') as file_name:
 	sky_data = np.loadtxt(file_name)
 
-#Pull timestamp from filename (there's probably a better way to do this)
+# Pull timestamp from filename (there's probably a better way to do this)
 header, *filename_parts = str(file_name).split('-')     
 file_name=str(filename_parts[2])
 header, *split = file_name.split('.')
@@ -27,15 +27,15 @@ el_end=int(scan_params[3])
 resolution=int(scan_params[4])
 
 
-#print ('az_start=',az_start,' az_end=',az_end,' el_start=',el_start,' el_end=',el_end,' res=',resolution)
+# Print ('az_start=',az_start,' az_end=',az_end,' el_start=',el_start,' el_end=',el_end,' res=',resolution)
 
 if resolution == 1: #standard low resolution
-	#Trim off the messy edges of the array (probably an ugly hack)
+	# Trim off the messy edges of the array (probably an ugly hack)
 	cleaned_data = np.delete(sky_data, obj=0, axis=0)
 	cleaned_data = np.delete(cleaned_data, obj=0, axis=1)
 	#cleaned_data = np.delete(cleaned_data, obj=(az_end-az_start-1), axis=1)
 	
-	#set up custom axis labels
+	# Set up custom axis labels
 	x=np.array([0,(az_end-az_start-1)/2,az_end-az_start-2])
 	az_range=np.array([az_end,(az_start+az_end)/2,az_start])
 	plt.xticks(x,az_range)
@@ -45,12 +45,12 @@ if resolution == 1: #standard low resolution
 	
 	
 elif resolution == 2:
-	#Trim off the messy edges of the array (probably an ugly hack)
+	# Trim off the messy edges of the array (probably an ugly hack)
 	cleaned_data = np.delete(sky_data, obj=0, axis=0)
 	cleaned_data = np.delete(cleaned_data, obj=0, axis=1)
 	#cleaned_data = np.delete(cleaned_data, obj=(az_end-((az_end-az_start)*5)-1), axis=1)
 	
-	#set up custom axis labels
+	# Set up custom axis labels
 	x=np.array([0,(((az_end-az_start)*5)-1)/2,((az_end-az_start)*5)-2])
 	az_range=np.array([az_end,(az_start+az_end)/2,az_start])
 	plt.xticks(x,az_range)
@@ -59,7 +59,7 @@ elif resolution == 2:
 	plt.yticks(y,el_range)
 
 else:
-	#medium resolution not implemented yet
+	# Medium resolution not implemented yet
 	print('')
 
 print('Processing heatmap...')
